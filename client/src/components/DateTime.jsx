7920d10ca64c66676f48d65e9c362ca9c98f9c88
Michael Ye <micxye@yahoo.com>
2018-05-23 11:29:41 -0700

    add google calendar

---
+++ client/src/components/DateTime.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+Number.prototype.pad = function (n) {
+    for (var r = this.toString(); r.length < n; r = 0 + r);
+    return r;
+};
+
+export default class DateTime extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    initClock() {
+        const updateClock = () => {
+            var now = new Date();
+            var milli = now.getMilliseconds(),
+                sec = now.getSeconds(),
+                min = now.getMinutes(),
+                hou = now.getHours(),
+                mo = now.getMonth(),
+                dy = now.getDate(),
+                yr = now.getFullYear();
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
+                corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
+            for (var i = 0; i < tags.length; i++)
+                document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+        }
+        updateClock();
+        window.setInterval(updateClock, 1);
+    }
+
+    componentDidMount() {
+        this.initClock()
+    }
+    
+    render() {
+        return (
+            <div id="datetime">
+                <a id="mon">January</a>
+                <a id="d">1</a>,
+                <a id="y">0</a><br />
+                <a id="h">12</a>:
+                <a id="m">00</a>:
+                <a id="s">00</a>
+                <a id="mi">000</a>
+            </div>
+        )
+    }
+}
+
+
+
+
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ToDoList from './ToDoList.jsx';
+import DateTime from './DateTime.jsx';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -11,8 +12,13 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div id="todolist">
-                <ToDoList />
+            <div id="appcontainer">
+                <div id="todolistcontainer">
+                    <ToDoList />
+                </div>
+                <div id="datetimecontainer">
+                    <DateTime />
+                </div>
             </div>
         )
     }
