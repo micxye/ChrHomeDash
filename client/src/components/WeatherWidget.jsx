fd8282a5bbc3fcdaab1f915d1a59a39e81c665a1
Michael Ye <micxye@yahoo.com>
2018-06-09 18:31:27 -0700

    add ip

--- client/src/components/WeatherWidget.jsx
+++ client/src/components/WeatherWidget.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import ip from 'ip';
+
 
 export default class WeatherWidget extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            ip: ip.address()
         }
     }
+
 
     render() {
         return (
             <div id="weatherwidget">
-
+                {this.state.ip}
             </div>
         )
     }
