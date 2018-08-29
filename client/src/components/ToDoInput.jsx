71bac42c8d58fb004c0db3e39a3e6f10a7be19ec
Michael Ye <micxye@yahoo.com>
2018-08-29 00:34:16 -0700

    add shia gif file, minor styling changes on todolist

--- client/src/components/ToDoInput.jsx
+++ client/src/components/ToDoInput.jsx
@@ -16,8 +16,8 @@ export default class ToDoInput extends Component {
         return (
             <form id="addtodobar">
                 <input id="addtodobutton" type="submit" value="+" onClick={(e) => { this.addNewToDo(e) }} />
-                <span id="addtodotext">{(() => this.props.count === 1 ? ` ${this.props.count} THING ` : ` ${this.props.count} THINGS`)()}</span>
-                <input id="todoinput" type="text" placeholder="New Thing" maxLength="52" autoComplete="off"/>
+                <span id="addtodotext">{(() => this.props.count === 1 ? `${this.props.count} THING ` : `${this.props.count} THINGS`)()}</span>
+                <input id="todoinput" type="text" placeholder="+ a new thing" maxLength="52" autoComplete="off"/>
             </form>
         )
     }
