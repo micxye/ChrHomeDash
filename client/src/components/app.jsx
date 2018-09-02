4efa358177912b68db3c678547bbacdfd6869676
Michael Ye <micxye@yahoo.com>
2018-09-02 12:13:08 -0700

    add tradingview widget + minor style adjustments

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -5,6 +5,7 @@ import WeatherWidget from './WeatherWidget.jsx';
 import TwitterFeed from './TwitterFeed.jsx';
 import HNFeed from './HNFeed.jsx';
 import InspiringQuote from './InspiringQuote.jsx';
+import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -28,8 +29,21 @@ export default class App extends React.Component {
                         <WeatherWidget />
                     </div>
                     <div id="calendarandfeedscontainer">
-                        <div id="calendarcontainer">
-                            <iframe id="calendar" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=michaelyeahh%40gmail.com&amp;color=%23A32929&amp;src=6r0la6ac4bnk4mjvri89dr59vs%40group.calendar.google.com&amp;color=%23528800&amp;ctz=America%2FLos_Angeles"></iframe>
+                        <div id="calendartradingviewcontainer">
+                            <div id="calendarcontainer">
+                                <iframe id="calendar" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=michaelyeahh%40gmail.com&amp;color=%23A32929&amp;src=6r0la6ac4bnk4mjvri89dr59vs%40group.calendar.google.com&amp;color=%23528800&amp;ctz=America%2FLos_Angeles"></iframe>
+                            </div>
+                            <div id="tradingviewcontainer">
+                                <TradingViewWidget
+                                    symbol="BITFINEX:BTCUSD"
+                                    range="1m"
+                                    style="3"
+                                    withdateranges="true"
+                                    theme={Themes.LIGHT}
+                                    locale="en"
+                                    autosize
+                                />
+                            </div>
                         </div>
                         <div id="feedscontainer">
                             <HNFeed />
