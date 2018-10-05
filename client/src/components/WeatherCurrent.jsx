bfab2b7a3d815c03fd3e2c5189da5b95d6a602cf
Michael Ye <micxye@yahoo.com>
2018-10-05 16:14:03 -0700

    re add current high low, move forecast dropdowns to the right

--- client/src/components/WeatherCurrent.jsx
+++ client/src/components/WeatherCurrent.jsx
@@ -25,6 +25,10 @@ export default class WeatherCurrent extends React.Component {
                     <div id="currenttempsummary">
                         {Math.round(this.props.weather.currently.temperature)}˚ {this.props.weather.currently.summary}  
                     </div>
+                    <div id="currenthighlow">
+                        High:&nbsp;{Math.round(this.props.weather.daily.data[0].temperatureHigh)}˚&nbsp;&nbsp;&nbsp;&nbsp;
+                        Low:&nbsp;{Math.round(this.props.weather.daily.data[0].temperatureLow)}˚
+                    </div>
                 </div>
             </div>
         )
